f2e9b11b854bf7512616f8b7826e701cad119b15
VexA3 <vexhd0@gmail.com>
2020-04-13 14:30:38 -0500

    Merge changes from Brian to Billy

--- Monopoly/Classes/Player.cs
+++ Monopoly/Classes/Player.cs
@@ -8,5 +8,13 @@ namespace Monopoly
 {
     class Player
     {
+        // True or false if the player is currently jailed.
+        public bool isJailed;
+
+        // what image the player selected.
+        // This doesn't have to be of type INT.
+        public int image;
+
+        public int moneyTotal;
     }
 }
